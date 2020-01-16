04fd88880f1b417c75ecbea80e13729e2d0a001e
dannyhuang <kuanhsuh@gmail.com>
2020-01-16 21:56:27 +0800

    How to Delete Old Photo From Server if User Change Profile Photo in Laravel 5.7 an Vue

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -76,7 +76,12 @@ class UserController extends Controller
             Image::make($request->photo)->save(public_path('img/profile/') . $name);
 
             $request->merge(['photo' => $name]);
-            // dd($request);
+
+            $userPhoto = public_path('img/profile/') . $currentPhoto;
+
+            if (file_exists($userPhoto)) {
+                unlink($userPhoto);
+            }
         }
 
         if (!empty($request->password)) {
